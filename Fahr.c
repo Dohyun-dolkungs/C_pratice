03d612f15619e7385e934bf5dc1d41f28ead29be
Dolkungs <dpj177tf@gmail.com>
2022-04-09 21:39:29 +0900

    4th commit heading added & opposite version added

--- Fahr.c
+++ Fahr.c
@@ -11,6 +11,8 @@ int main() {
 
     fahr = lower;
 
+    // heading
+    printf("F-celsius to C-celsius\n");
     while(fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
